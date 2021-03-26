d028f5e6ed521e51944e92e8d1fbf798aaf3ab4d
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2021-03-26 23:39:43 +0100

    feat: solve extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (typeof str !== "string") str = "" + str;
+
+  let repeatTimes = 1;
+  if (options.repeatTimes) repeatTimes = options.repeatTimes;
+
+  let separator = "+";
+  if (options.separator) separator = options.separator;
+
+  let addition = "";
+  if (
+    options.addition ||
+    options.addition === false ||
+    options.addition === null
+  ) {
+    addition = options.addition;
+    if (typeof addition !== "string") addition = "" + addition;
+  }
+
+  let additionRepeatTimes = 1;
+  if (options.additionRepeatTimes)
+    additionRepeatTimes = options.additionRepeatTimes;
+
+  let additionSeparator = "|";
+  if (options.additionSeparator) additionSeparator = options.additionSeparator;
+
+  let result = "" + str;
+
+  if (additionRepeatTimes > 1) {
+    let tempArr = [];
+    console.log(addition);
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      tempArr.push(addition);
+    }
+    result += tempArr.join(additionSeparator);
+  } else result = str + addition;
+
+  if (repeatTimes > 1) {
+    let tempArr = [];
+    for (let i = 0; i < repeatTimes; i++) {
+      tempArr.push(result);
+    }
+    console.log(tempArr);
+    result = tempArr.join(separator);
+  }
+
+  return result;
 };
-  
